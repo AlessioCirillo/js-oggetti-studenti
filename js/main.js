d9096fd2fc6e67db83e39af3cddc638bc64a9b14
alessio <alessio.cirillo.1995@gmail.com>
2020-10-30 15:50:00 +0100

    oggetto

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//CREAZIONE E STAMPA OGGETTO
+var oggetto = {
+    nome : 'alessio',
+    cognome : 'cirillo',
+    anni : 25
+};
+
+for (var key in oggetto){
+    console.log(oggetto[key]);
+};
+
+
+//
