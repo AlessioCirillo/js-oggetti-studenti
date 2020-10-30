ed9ed9959265796c0311d824329d70540f896144
alessio <alessio.cirillo.1995@gmail.com>
2020-10-30 16:18:57 +0100

    js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
 //CREAZIONE E STAMPA OGGETTO
-var oggetto = {
-    nome : 'alessio',
-    cognome : 'cirillo',
-    anni : 25
-};
+// var oggetto = {
+//     nome : 'alessio',
+//     cognome : 'cirillo',
+//     anni : 25
+// };
 
-for (var key in oggetto){
-    console.log(oggetto[key]);
-};
+// for (var key in oggetto){
+//     console.log(oggetto[key]);
+// };
 
 
-//
+//ARRAY OGGETTI
+var array = [
+    {
+        nome : 'alessio',
+        cognome : 'cirillo',
+        anni : 25
+    },
+    {
+        nome : 'paolo',
+        cognome : 'duzioni',
+        anni : 30
+    }
+]
+
+
+var nome = prompt( 'inserisci nome' );
+var cognome = prompt( 'inserisci cognome' );
+var anni = parseInt ( prompt ( 'inserisci la tua età' ) );
+
+var oggettiUser = {};
+
+console.log(array);
